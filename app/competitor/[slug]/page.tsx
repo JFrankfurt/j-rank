48433525e38a79e40d374476b9164c72d1598638
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2023-12-25 15:36:52 -0600

    add uuid lookup

---
+++ app/competitor/[slug]/page.tsx
@@ -0,0 +1,26 @@
+import { Competitor } from '@/app/components/Athletes'
+import styles from './page.module.scss'
+import { NextPageContext } from 'next'
+
+interface CompetitorPageProps {
+  params: { slug: string }
+}
+
+export default function Competitor({ params: { slug } }: CompetitorPageProps) {
+  return (
+    <main className={styles.root} id={slug}>
+      <h1>Competitor name</h1>
+      <p>{slug}</p>
+    </main>
+  )
+}
+
+export async function generateStaticParams() {
+  const competitors: Competitor[] = await fetch('https://.../competitors').then(
+    (res) => res.json()
+  )
+
+  return competitors.map((competitor) => ({
+    slug: competitor.uuid as string,
+  }))
+}
